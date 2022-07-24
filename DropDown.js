b252fb2fac55fb209d3d540e2d18701a009b8fa7
iulia danilova <101838205+Iulia77@users.noreply.github.com>
2022-07-24 22:18:42 +0300

    Update DropDown.js

--- DropDown.js
+++ DropDown.js
@@ -7,6 +7,20 @@ class DropDown {
          this.items = items
          this.id = id
          this.state_open = false
+
+         this.styles = {
+             palette: {
+                default: {
+                    background: '#123',
+                    color: '#fff',
+                },
+                  active: {
+                    background: '#eee',
+                    color: '#234', 
+                  },  
+                  
+             }
+         }
     }
 
     render() {
@@ -14,19 +28,31 @@ class DropDown {
         
 
         let rootDiv = document.getElementById(this.id)
+        
+        rootDiv.style.display = 'inline-block'
+        rootDiv.style.fontFamily = 'Arial'
+
+
         rootDiv.innerHTML = `
-        <a href='#'>
+        <a href='#' style="background: ${this.styles.palette.default.background};
+                        color: ${this.styles.palette.default.color};
+                        padding: 1em;
+                        text-decoration: none;
+                        display: block;
+                        ">
         ${this.items.root.label}
         </a>
         `
-        let rootLink = document.querySelector(`#${this.id} > a`)
-        rootLink.addEventListener('click', this.handler.bind(this))
-
+        
         if (this.state_open) {
             let html = ``
             for (let i=0; i<this.items.children.length; i++) {
                 html += `
-                <a href='#'>
+                <a href='#' class="item" style="background: ${this.styles.palette.default.background};
+                        color: ${this.styles.palette.default.color};
+                        padding: 1em;
+                        text-decoration: none;
+                        display: block;">
                 ${this.items.children[i].label}
                 </a>
                 `
@@ -35,24 +61,55 @@ class DropDown {
             rootDiv.innerHTML += html
         }
 
-      
+        let rootLink = document.querySelector(`#${this.id} > a`)
+        let items = document.querySelectorAll(`#${this.id} > .item`)
 
-    }
-
-    handler() { 
-        console.log(this)
-        this.state_open = true 
-        this.render()
         
-         //toggling
-        //HW1: upgrade the handler's logic,
-        // so it opens at first click and closes at the second one.
-    }
+        for (let i=0; i<items.length; i++) {
+            items[i].addEventListener('mouseover', this.mouseoverHandler.bind(this));
+            
+            
+        }
+            
+        rootLink.addEventListener('click', this.clickHandler.bind(this))
+
+
+      
+    }  
 
     
-}
+    clickHandler() {
 
+        //HW2: How to make this code compact
+            this.state_open ? this.state_open = false : this.state_open = true
+              
+            this.render()
+        }
 
+        mouseoverHandler(e) {
+             e.target.style.color = `${this.styles.palette.active.color} `
+             e.target.style.background = `${this.styles.palette.active.background} `
+        }
+//HW3: create and bind the mouseoutHandler(e)
+// restores the default styles
+         mouseoutHandler(e) {
+             e.target.style.color = `${this.styles.palette.active.color} `
+             e.target.style.background = `${this.styles.palette.active.background} `
+         }
+
+    }
+             //toggling
+            //HW1: upgrade the handler's logic,
+            // so it opens at first click and closes at the second one.
+     
+/*handler() {
+        if (this.state_open) {
+            this.state_open = false
+        } else {
+            this.state_open = true
+        }
+        
+        this.render() */
 
 //declare a function () => { console.log(1) }
 //call a function
